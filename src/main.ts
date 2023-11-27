225681dfe2c14e2c6ec7f28163c87feca914d0a4
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-11-27 18:42:37 -0300

    feat: instalando lib de segurança no projeto

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 const PORT = Number(process.env.PORT) || 3001;
 
@@ -31,6 +32,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.use(helmet());
+
   await app.listen(PORT);
 }
 bootstrap();
